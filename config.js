3d380f1fcc1b6d258d3fdb711c376fc6bf5763e3
argus12345 <rty09371@gmail.com>
2024-09-02 01:38:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "3U13EIRY#7IsY3Lhfj8Zi_WC31QkGbE9g1eIBA7YmKLQvroaq5ZQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9b44e3266c065df601430.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
 SUDO_NB: process.env.SUDO_NB || "94718913389",
